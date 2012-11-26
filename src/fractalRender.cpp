f0c7cc9b133764ebda38b5eb814f5f93d0c30767
Travace Reese <tsuk13@gmail.com>
2012-11-26 01:19:38 -0500

    3d fractal rendering appears to be working

--- src/fractalRender.cpp
+++ src/fractalRender.cpp
@@ -17,7 +17,8 @@ using std::vector;
 #define window_height 480
 
 //Fractal Information
-vector<TwoDStruct*> imageVec;
+vector<TwoDStruct*> twoDImageVec;
+vector<ThreeDStruct*> threeDImageVec;
 int numPoints = 10000; //number of points used in fractal
 int deep = 15; //how many iterations of rules
 double size = 2; // size of points
@@ -44,10 +45,12 @@ bool events()
 
 void main_loop_function()
 {
-   //float angle = 0;
+   //control vars
    float zoom = 1;
    float tranX = 0;
    float tranY = 0;
+   float rotX = 0;
+   float rotY = 0;
    while( events() )
    {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -64,10 +67,10 @@ void main_loop_function()
            //this is our process to create fractal
            //loop to apply random transformations of the inputed ones
            for(int j = 0; j<deep; j++){ 
-             int r = rand() % imageVec.size();
-             glTranslatef(imageVec[r]->getTranX(),imageVec[r]->getTranY(),0);
-             glScalef(imageVec[r]->getScale(), imageVec[r]->getScale(), 1);
-             glRotatef(-imageVec[r]->getRotate(), 0, 0, 1); 
+             int r = rand() % twoDImageVec.size();
+             glTranslatef(twoDImageVec[r]->getTranX(),twoDImageVec[r]->getTranY(),0);
+             glScalef(twoDImageVec[r]->getScale(), twoDImageVec[r]->getScale(), 1);
+             glRotatef(-twoDImageVec[r]->getRotate(), 0, 0, 1); 
            }
            //Draw
            glBegin(GL_QUADS);
@@ -80,6 +83,58 @@ void main_loop_function()
 
          //code for threeD specific render
          else{
+           //Camera
+           glScalef(zoom, zoom, 1);
+           glTranslatef(0, 0, -5);
+           glRotatef(rotX, 1, 0, 0);
+           glRotatef(rotY, 0, 1, 0);
+           
+           //this is our process to create fractal
+           //loop to apply random transformations of the inputed ones
+           for(int j = 0; j<deep; j++){ 
+             int r = rand() % threeDImageVec.size();
+             glTranslatef(threeDImageVec[r]->getTranX(),threeDImageVec[r]->getTranY(), threeDImageVec[r]->getTranZ());
+             glScalef(threeDImageVec[r]->getScaleX(), threeDImageVec[r]->getScaleY(), threeDImageVec[r]->getScaleZ()); 
+             glRotatef(-threeDImageVec[r]->getRotateZ(), 0, 0, 1);
+             glRotatef(-threeDImageVec[r]->getRotateY(), 0, 1, 0);
+             glRotatef(-threeDImageVec[r]->getRotateX(), 1, 0, 0);
+           }
+           
+           //Draw
+           glBegin(GL_QUADS);
+           glColor3ub(255, 255, 255);
+           //front face
+           glVertex3f( 0,  -size, 0);
+           glVertex3f( 0,  0, 0);
+           glVertex3f( size,  0, 0);
+           glVertex3f( size, -size, 0);
+           //right face
+           glVertex3f( size,  0, 0);
+           glVertex3f( size,  -size, 0);
+           glVertex3f( size, -size, size);
+           glVertex3f( size,  0, size);
+           //back face
+           glVertex3f( size,  -size, size);
+           glVertex3f( size,  0, size);
+           glVertex3f( 0,  0, size);
+           glVertex3f( 0, -size, size);
+           //left face
+           glVertex3f( 0,  0, size);
+           glVertex3f( 0,  -size, size);
+           glVertex3f( 0, -size, 0);
+           glVertex3f( 0, 0, 0);
+           //top face
+           glVertex3f( 0,  0, 0);
+           glVertex3f( size,  0, 0);
+           glVertex3f( size, 0, size);
+           glVertex3f( 0, 0, size);
+           //bottum face
+           glVertex3f( 0,  -size, 0);
+           glVertex3f( size,  -size, 0);
+           glVertex3f( size, -size, size);
+           glVertex3f( 0, -size, size);
+           
+           glEnd();
          }
       }
       SDL_GL_SwapBuffers();
@@ -95,6 +150,10 @@ void main_loop_function()
       }
       //threeD specfic controls
       else{
+        if( key[SDLK_w] ){ rotX -= .2; } 
+        if( key[SDLK_s] ){ rotX += .2; }
+        if( key[SDLK_a] ){ rotY += .2; }
+        if( key[SDLK_d] ){ rotY -= .2; }
       }
    }
 }
@@ -137,10 +196,39 @@ int main()
        float tY;
        cin >> tY;
        //input all this info into the structure object
-       imageVec.push_back(new TwoDStruct(s,r,tX,tY));
+       twoDImageVec.push_back(new TwoDStruct(s,r,tX,tY));
      }
      //threeD specfic input
      else{
+       cout << "\nInput image " << i << "'s X scale factor: ";
+       float sX;
+       cin >> sX;
+       cout << "Input image " << i << "'s Y scale factor: ";
+       float sY;
+       cin >> sY;
+       cout << "Input image " << i << "'s Z scale factor: ";
+       float sZ;
+       cin >> sZ;
+       cout << "Input image " << i << "'s Clockwise rotation(degrees) about X-axis: ";
+       float rX;
+       cin >> rX;
+       cout << "Input image " << i << "'s Clockwise rotation(degrees) about Y-axis: ";
+       float rY;
+       cin >> rY;
+       cout << "Input image " << i << "'s Clockwise rotation(degrees) about Z-axis: ";
+       float rZ;
+       cin >> rZ;
+       cout << "Input image " << i << "'s translation in X: ";
+       float tX;
+       cin >> tX;
+       cout << "Input image " << i << "'s translation in Y: ";
+       float tY;
+       cin >> tY;
+       cout << "Input image " << i << "'s translation in Z: ";
+       float tZ;
+       cin >> tZ;
+       //input into struct
+       threeDImageVec.push_back(new ThreeDStruct(sX,sY,sZ,rX,rY,rZ,tX,tY,tZ));
      }
    }
    cout << "\nCustom Render Settings?[y/n]";
